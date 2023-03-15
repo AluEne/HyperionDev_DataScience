25d3960d69d2a2988d7a5ccd62e7a60b5786c101
AluEne <111243104+AluEne@users.noreply.github.com>
2023-03-15 18:04:13 +0000

    Order Confirmation Commit

---
+++ menu.py
@@ -0,0 +1,14 @@
+# Ask user to order thier three favourite meals, store them in seprate variables
+item1 = input("Kindly input your first order: ")
+
+item2 = input("Kindly input your second order: ")
+
+item3 = input("Kindly input your third order: ")
+
+# Print them out seprately 
+
+print("Order Confirmation! You have orderd")
+
+print(item1)
+print(item2)
+print(item3)
