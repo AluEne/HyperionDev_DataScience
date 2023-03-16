9654f128b54321093cbd46857060cd7aae75c3c9
AluEne <111243104+AluEne@users.noreply.github.com>
2023-03-16 13:28:13 +0000

    replace commit

---
+++ replace.py
@@ -0,0 +1,8 @@
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog!."
+
+new_sentence = sentence.replace("!" , " ")
+print(new_sentence)
+
+print(new_sentence.upper())
+
+print(new_sentence[::-1])
